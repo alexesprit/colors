9aa242f4fcf72bd9b3c795f148f34048f0bf3213
alexesprit <alex.esprit@gmail.com>
2020-11-04 13:52:26 +0300

    Add missing dots

--- transform.v
+++ transform.v
@@ -18,29 +18,29 @@ pub fn (val RGB) invert() RGB {
 	}
 }
 
-// lighten changes lightness of a given HSL struct and returns new struct
+// lighten changes lightness of a given HSL struct and returns new struct.
 pub fn (val HSL) lighten(value f64) HSL {
 	l := between(val.l + value, 0, 1)
 	return HSL{val.h, val.s, l}
 }
 
-// darken changes lightness of a given HSL struct and returns new struct
+// darken changes lightness of a given HSL struct and returns new struct.
 pub fn (val HSL) darken(value f64) HSL {
 	return val.lighten(-value)
 }
 
-// saturate changes saturation of a given HSL struct and returns new struct
+// saturate changes saturation of a given HSL struct and returns new struct.
 pub fn (val HSL) saturate(value f64) HSL {
 	s := between(val.s + value, 0, 1)
 	return HSL{val.h, s, val.l}
 }
 
-// desaturate changes saturation of a given HSL struct and returns new struct
+// desaturate changes saturation of a given HSL struct and returns new struct.
 pub fn (val HSL) desaturate(value f64) HSL {
 	return val.saturate(-value)
 }
 
-// rotate changes hue of a given HSL struct and returns new struct
+// rotate changes hue of a given HSL struct and returns new struct.
 pub fn (val HSL) rotate(degrees int) HSL {
 	h := int(math.abs(val.h + degrees)) % 360
 	return HSL{h, val.s, val.l}
